481afcb4fd03260a6ed01b09c966bcb0f60cea79
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-26 16:50:03 -0400

    staged for deploy

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -4,7 +4,7 @@
 module.exports = ({ env }) => ({
   proxy: true,
   host: "127.0.0.1" || "0.0.0.0",
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || env.int('DATABASE_PORT'),
   url: env('MY_HEROKU_URL'),
   app: { 
     keys: env.array('APP_KEYS') || [env('APP_KEY1'), env('APP_KEY2')]
