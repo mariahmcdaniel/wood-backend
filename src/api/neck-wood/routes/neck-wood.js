5ed199c7a80a1976d50e20c347797c6839f6bb07
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-25 21:29:41 -0400

    setting up db

---
+++ src/api/neck-wood/routes/neck-wood.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * neck-wood router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::neck-wood.neck-wood');
---
+++ src/api/neck-wood/services/neck-wood.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * neck-wood service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::neck-wood.neck-wood');
