2d52c2b0ba9f68bac04ca374a8737f7113a53ded
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-27 23:01:24 -0400

    resolving errors

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,14 +1,9 @@
 module.exports = ({ env }) => ({
+  host: env("DATABASE_HOST", "0.0.0.0"),
+  port: process.env.PORT || env.int('DATABASE_PORT'),
+  url: env("MY_HEROKU_URL", "https://eric-wood-guitars-backend.herokuapp.com/"),
   proxy: true,
-  host: "0.0.0.0" || process.env.HOST,
-  port: process.env.PORT || 5432,
-  url: env('MY_HEROKU_URL'),
-  app: { 
-    keys: env.array('APP_KEYS') || [env('APP_KEY1'), env('APP_KEY2')]
+  app: {
+    keys: env.array("APP_KEYS")|| [env("APP_KEY1"), env("APP_KEY2")],
   },
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET'),
-    },
-  },
-})
+});
