e60e8e82dd6fe33b234eef63718fbc7b190fb267
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-28 00:56:22 -0400

    added public_url

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   port: process.env.PORT || env.int('DATABASE_PORT'),
-  url: env('PUBLIC_URL'),
+  url: env('PUBLIC_URL', 'https://eric-wood-guitars-backend.herokuapp.com/admin'),
   proxy: true,
   app: {
     keys: env.array("APP_KEYS")|| [env("APP_KEY1"), env("APP_KEY2")],
