590d8f60e28877bd3d3dc742b84528e4cb0dc379
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-28 00:24:30 -0400

    set up db function

--- config/database.js
+++ config/database.js
@@ -24,11 +24,7 @@ module.exports = ({ env }) => {
     connection: {
     client: 'postgres',
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
+      connectionString: process.env.DATABASE_URL,
       ssl: {
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
       },
