37d9cadbe9374b00cd0883c535c208140bfacb7c
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-27 21:59:36 -0400

    resolving errors

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   proxy: true,
   host: "127.0.0.1" || "0.0.0.0",
-  port: process.env.PORT || env.int('DATABASE_PORT'),
+  port: process.env.PORT || 5432,
   url: env('MY_HEROKU_URL'),
   app: { 
     keys: env.array('APP_KEYS') || [env('APP_KEY1'), env('APP_KEY2')]
