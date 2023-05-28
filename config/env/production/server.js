322735caad8c9531d91b042eebd8b793cffb19ff
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-28 00:39:25 -0400

    rm host from prod server

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,5 +1,4 @@
 module.exports = ({ env }) => ({
-  host: env("DATABASE_HOST", "0.0.0.0"),
   port: process.env.PORT || env.int('DATABASE_PORT'),
   url: env("MY_HEROKU_URL", "https://eric-wood-guitars-backend.herokuapp.com/"),
   proxy: true,
