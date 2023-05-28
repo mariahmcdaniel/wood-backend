0e6502b0f5f81ee17dbdf8a7302236e502270505
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-28 00:52:34 -0400

    rm host from prod server

--- config/database.js
+++ config/database.js
@@ -11,6 +11,7 @@ module.exports = ({ env }) => {
       host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'wood_luth_db'),
+      url: env('PUBLIC_URL'),
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', '0000'),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   port: process.env.PORT || env.int('DATABASE_PORT'),
-  url: env("MY_HEROKU_URL", "https://eric-wood-guitars-backend.herokuapp.com/"),
+  url: env('PUBLIC_URL'),
   proxy: true,
   app: {
     keys: env.array("APP_KEYS")|| [env("APP_KEY1"), env("APP_KEY2")],
